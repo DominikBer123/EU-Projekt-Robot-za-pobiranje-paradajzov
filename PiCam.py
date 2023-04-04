b1b5cb19c4c5b81e79c84f5c8f52ced2967ae764
Dominik Bernik <dominik.bernik@gmail.com>
2023-04-04 21:47:52 +0200

    kamera in Callback
    Pravilno delovanje kamere in callback funkcije
    
    Treba dodati še:
    - servo funkcijo,
    - spremeniti, da shrani v .mjpeg datoteki
    - spremeniti na 10 FPS

--- PiCam.py
+++ PiCam.py
@@ -1,59 +1,33 @@
-#import io
-#import time
-#import picamera
-#from PIL import Image
-#
-#def outputs():
-#    stream = io.BytesIO()
-#    for i in range(10):
-#        # This returns the stream for the camera to capture to
-#        yield stream
-#        # Once the capture is complete, the loop continues here
-#        # (read up on generator functions in Python to understand
-#        # the yield statement). Here you could do some processing
-#        # on the image...
-#        #stream.seek(0)
-#        #img = Image.open(stream)
-#        # Finally, reset the stream for the next capture
-#        stream.seek(0)
-#        stream.truncate()
-#
-#with picamera.PiCamera() as camera:
-#    camera.resolution = (640, 480)
-#    camera.framerate = 10
-#    time.sleep(2)
-#    start = time.time()
-#    camera.capture_sequence(outputs(), 'jpeg', use_video_port=True)
-#    finish = time.time()
-#    print('Captured 40 images at %.2ffps' % (10 / (finish - start)))
-#
-#    print("hope it works")
-#
-#
+
+
+##===================================================================================##
+##======================================DELA H264====================================##
+##===================================================================================##
 
 import time
-import picamera
 
-def premik_servo():
-    strat = time.time()
-    time.sleep(0.5)
-    stop = time.time()
-    print(stop-strat)
-    print("-------------------")
-    
-    
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+i = 0
 
-with picamera.PiCamera() as camera:
+def ola(a):
+    print(a)
+    global i
+    i=i+1
 
-    camera.start_preview()
-    camera.start_recording('video.mjpg')
-    
-    
-    time.sleep(2)
-    
-    
 
-    camera.stop_recording()
-    camera.stop_preview()
+picam2 = Picamera2()
+picam2.framerate = 10
+picam2.pre_callback =ola(i)
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+
+encoder = H264Encoder(10000000)
+
+picam2.start_recording(encoder, 'test.h264')
+time.sleep(10)
+picam2.stop_recording()
+
+
 
 
